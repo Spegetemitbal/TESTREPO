5e42863f3d077c70361fa10c8cc9a9df744630ad
Evan <LeSkree@gmail.com>
2017-04-24 12:57:59 -0700

    no message

---
+++ GitIgnoreProject/Assets/poop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class poop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
